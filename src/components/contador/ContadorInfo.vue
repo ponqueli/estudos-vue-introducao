05a628f7154f6d8fedce1f8703853d3e79ee6c2d
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2021-12-18 11:34:51 -0300

    data como funcao, registrar glob e localmente, boas praticas nomeacao de arquivos e seletores, estrutura de diretorios e estilo css scoped e module

---
+++ src/components/contador/ContadorInfo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div :class="$style.contadorInfo">
+        <h2>Contador Vue</h2>
+        <hr>
+    </div>
+</template>
+<style module>
+    .contadorInfo{
+        border: 1px solid green;
+    }
+
+</style>
---
+++ src/components/shared/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Curso VueJS</h1>
+    </div>
+</template>
+
